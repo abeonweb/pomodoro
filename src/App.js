f2fa58d46827bef3ebe58f09b55b33de4e5d39f5
abeonweb <bmospecific@yahoo.com>
2022-10-25 22:21:13 +0100

    update the app component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import Pomodoro from './Pomodoro';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>
+          Pomodoro Timer
+        </h1>
       </header>
+      <div>
+          {/* cycle count dropdown */}
+          {/* work time dropdown */}
+          {/* rest time dropdown */}
+          {/* start button */}
+      </div>
+      <div>
+        {/* open a modal */}
+        {/* run the pomodoro for x cycles */}
+        <Pomodoro />
+      </div>
     </div>
   );
 }
