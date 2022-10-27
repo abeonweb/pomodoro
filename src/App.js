dc09e374595d5d8e9206554b34528d2239f1cb9c
abeonweb <bmospecific@yahoo.com>
2022-10-27 12:38:41 +0100

    included several dropdwns for more functionality

--- src/App.js
+++ src/App.js
@@ -1,7 +1,35 @@
+import  usePomodoroData from "./usePomodoroData"
 import './App.css';
 import Pomodoro from './Pomodoro';
+import Dropdown from "./Dropdown";
 
 function App() {
+
+const [state, dispatch] = usePomodoroData()
+  
+  const cycleOptions = {
+    name: "Cycles",
+    values: [2, 3, 4, 5],
+    type: "CYCLE"
+  }
+
+  const workOptions = {
+    name: "Work",
+    values: [25, 30, 40, 45],
+    type: "WORK"
+  }
+
+  const restOptions = {
+    name: "Rest",
+    values: [5, 10, 15],
+    type: "REST"
+  }
+
+  const handleStart = () => {
+    dispatch({type: "CYCLE"})
+    console.log(state)
+  }
+
   return (
     <div className="App">
       <header className="App-header">
@@ -10,11 +38,19 @@ function App() {
         </h1>
       </header>
       <div>
-          {/* cycle count dropdown */}
-          {/* work time dropdown */}
-          {/* rest time dropdown */}
-          {/* start button */}
+        {/* cycle count dropdown */}
+        <Dropdown options ={cycleOptions} dispatch={dispatch}/>
+
+        {/* work time dropdown */}
+        <Dropdown options ={workOptions} dispatch={dispatch}/>
+
+        {/* rest time dropdown */}
+        <Dropdown options ={restOptions} dispatch={dispatch}/>
+
       </div>
+        {/* start button */}
+      <button onClick={handleStart}>Start</button>
+      
       <div>
         {/* open a modal */}
         {/* run the pomodoro for x cycles */}
@@ -23,5 +59,7 @@ function App() {
     </div>
   );
 }
+
+
 
 export default App;
